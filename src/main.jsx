1d5aecf7ba40bb8c24804177ac7641f0e635c392
Ahmed Talaat <ahmadalazab1592@gmail.com>
2026-07-25 23:53:15 +0300

    تفعيل الإحصائيات

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// 1. أضف سطر الاستدعاء هذا هنا
+import { Analytics } from '@vercel/analytics/react' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* 2. أضف هذا السطر هنا أسفل التطبيق مباشرة */}
+    <Analytics />
   </StrictMode>,
 )
